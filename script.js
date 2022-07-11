208f950fc0055be518af701bf4bc6b713be76306
Vinicius Kaya <viniciuskaya87@gmail.com>
2022-07-11 16:50:27 -0300

    Foi criado o design do site ultilizando o gimp e salvo no repositorio, design usado para referencia

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 chamarApi();
 
 async function chamarApi() {
-  let url = `https://rickandmortyapi.com/api/character?name=bird`; 
+  let url = `https://rickandmortyapi.com/api/episode/3`; 
 
     let results = await fetch(url);
     let json = await results.json();
