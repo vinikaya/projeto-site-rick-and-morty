4124031b73b057a48647984d757f28d2b89e9fa9
Vinicius Kaya <viniciuskaya87@gmail.com>
2022-07-12 18:19:33 -0300

    Estilizei o header, e comecei a section principal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 chamarApi();
 
 async function chamarApi() {
-  let url = `https://rickandmortyapi.com/api/episode/3`; 
+  let url = `https://rickandmortyapi.com/api/character/1`; 
 
     let results = await fetch(url);
     let json = await results.json();
