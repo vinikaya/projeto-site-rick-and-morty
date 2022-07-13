18dba46061f35512ee0752ad767f36707a59cef5
Vinicius Kaya <viniciuskaya87@gmail.com>
2022-07-13 16:31:29 -0300

    começo do javascript, aprendendo a fazer um codigo que clone elemntos do html

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-chamarApi();
+document.querySelector('#buscar').addEventListener("click", (event)=> {
+  event.preventDefault();
 
+  var nomeApi = document.querySelector('#entradaDados').value; 
+  console.log(nomeApi);
+
+})
+
+
+chamarApi();
 async function chamarApi() {
   let url = `https://rickandmortyapi.com/api/character/1`; 
 
     let results = await fetch(url);
     let json = await results.json();
-    console.log(json);  
+    
+    json.map((item,index)=> {
+      let pizzaItem = document.querySelector('.models .container').cloneNode(true);
+
+
+    })
+
 }
