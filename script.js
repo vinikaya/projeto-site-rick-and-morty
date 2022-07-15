ce5d8cb365aaf96e56fa719b4bfe45b6fe8c5c7f
Vinicius Kaya <viniciuskaya87@gmail.com>
2022-07-15 17:27:08 -0300

    Foi criado o codigo JS que clona a estrutura modelo e exibe na tela com os itens preenchidos com o nome dos personagens encontrados, porem foi detectado um erro ao tentar expandir as informações de cda personagem, não altera o valor de 0px para 100px do .hid, pois não encontrei um jeito de fazer o java script manipular elementos em tempo real, esse erro se dá pois os elementos foram craidos posteriormente e a pagiona fez a leitura do codigo e não reconheceu esses elementos pois ainda não haviam sido criados, uma solução seria criar uma nova brach e mudar a estrutura de visualização dos personagens ou achar uma forma de fazer a pagina ler o  codigo em tempo real

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
+
 document.querySelector('#buscar').addEventListener("click", (event)=> {
   event.preventDefault();
-
   var nomeApi = document.querySelector('#entradaDados').value; 
   console.log(nomeApi);
-
+  chamarApi(nomeApi);
+  disable();
 })
 
-
-chamarApi();
-async function chamarApi() {
-  let url = `https://rickandmortyapi.com/api/character/1`; 
+async function chamarApi(nome) {
+  let url = `https://rickandmortyapi.com/api/character/?name=${nome}`; 
 
     let results = await fetch(url);
     let json = await results.json();
     
-    json.map((item,index)=> {
-      let pizzaItem = document.querySelector('.models .container').cloneNode(true);
 
+    json.results.map((item,index)=>{
+      //clonando a estrutura modelo dos personagens
+      charItem = document.querySelector('.model .container').cloneNode(true);
+      
+      //modificando os elementos dentro da estrutura de acordo com o objeto recebido
+      charItem.querySelector('.a1 span').innerHTML = item.name;
+      charItem.querySelector('.a2 span').innerHTML = item.status;
+      if(item.status === 'alive'){
+        charItem.querySelector('.a2 .status-alive').style.backgroundColor = 'green';
+      } else if(item.status === 'Dead') {
+        charItem.querySelector('.a2 .status-alive').style.backgroundColor= 'red';
+      } else if(item.status === 'unknown') {
+        charItem.querySelector('.a2 .status-alive').style.backgroundColor = 'grey';
+      }
 
-    })
+      charItem.querySelector('img').src = item.image;
+      charItem.querySelector('.gender span').innerHTML = item.gender;
+      charItem.querySelector('.especie span').innerHTML = item.species;
+      charItem.querySelector('.ultimo-local span').innerHTML = item.location.name;
+      charItem.querySelector('.local-origem span').innerHTML = item.origin.name;
 
+      //exibindo na tela a estrutura clonada
+      document.querySelector('.charArea').append(charItem);
+
+      
+    });
+    
+} 
+
+document.querySelector('.reload').addEventListener("click",()=>{reset()});
+
+function disable() {
+  document.querySelector('#buscar').disabled = true;
+  document.querySelector('#buscar').style.backgroundColor = "#3d3d3d";
+  document.querySelector('#buscar').style.cursor = "default";
+  document.querySelector('.reload').style.display = 'block';
+  document.querySelector('.reload').style.opacity = '1';
+  document.querySelector('#entradaDados').disabled = true;
+}
+function reset() {
+  location.reload();
 }
